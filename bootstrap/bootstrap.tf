3c8ae0de9460ef5a104b5e1fb8d2ef4f03c0afa1
Glen Jarvis <glen@glenjarvis.com>
2026-03-17 17:44:08 -0700

    Move to S3 backend for locking
    Fix: #6

--- bootstrap/bootstrap.tf
+++ bootstrap/bootstrap.tf
@@ -67,21 +67,6 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "terraform_db_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 output "bootstrap_s3_save_state" {
   description = "Command to save state for future use"
   value       = <<-EOF
@@ -100,8 +85,8 @@ output "project_s3_configuration" {
         bucket         = "${local.state_bucket}"
         key            = "global/k8s/terraform.tfstate"
         region         = "${local.k8s_region}"
-        dynamodb_table = "terraform-lock"
         encrypt        = true
+        use_lockfile   = true
     }
     EOF
 }
